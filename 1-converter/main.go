ab79999cb7cb6d2e59d900d9dcc3e0ff57a78511
cobidez <cobidez911@yandex.ru>
2025-07-11 22:00:54 +0200

    add functions

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,9 +1,33 @@
 package main
 
+import "fmt"
+
 func main() {
 	const (
-		EurInUds float64 = 1.17
+		EurInUsd float64 = 1.17
 		RubInUsd float64 = 0.013
 		EurInRub float64 = 1.17 / 0.013
 	)
+}
+
+func getUserInputData() (string, float64, string) {
+	var (
+		from   string
+		amount float64
+		to     string
+	)
+	fmt.Print("Введите валюту, из которой Вы хотите конвертировать: ")
+	fmt.Scan(&from)
+
+	fmt.Print("Введите сумму конвертации: ")
+	fmt.Scan(&amount)
+
+	fmt.Print("Введите валюту, в которую Вы хотите конвертировать: ")
+	fmt.Scan(&to)
+
+	return from, amount, to
+}
+
+func calculate(from string, amount float64, to string) float64 {
+
 }
