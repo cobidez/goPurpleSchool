10cb88f1e61f8855909eb652894745c028c144d4
cobidez <cobidez911@yandex.ru>
2025-07-11 20:42:14 +0200

    Fix

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,8 +2,8 @@ package main
 
 func main() {
 	const (
-		UsdToEur float64 = 1.17
-		UsdToRub float64 = 0.013
-		RubToEur float64 = 1.17 / 0.013
+		EurInUds float64 = 1.17
+		RubInUsd float64 = 0.013
+		EurInRub float64 = 1.17 / 0.013
 	)
 }
