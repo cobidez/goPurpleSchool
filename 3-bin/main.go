6b10b6393dffbf7fa3da9ca22b4c1cde7c30473c
cobidez <cobidez911@yandex.ru>
2025-08-10 14:29:15 +0200

    [3-bin] start

---
+++ 3-bin/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
+// Types
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+type BinList []Bin
+
+// Funcs
+func newBin(name string) (*Bin, error) {
+	if strings.TrimSpace(name) == "" {
+		return nil, errors.New("NAME_IS_EMPTY")
+	}
+
+	return &Bin{
+		name:      name,
+		createdAt: time.Now(),
+	}, nil
+}
+
+func main() {
+}
